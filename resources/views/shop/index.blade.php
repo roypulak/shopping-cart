fed2ea448d79e6abc5c542e368fb505062ac716c
Pulak Roy <roy1pulak@gmail.com>
2017-10-08 08:28:43 +0200

    Outoutting Product Data

--- resources/views/shop/index.blade.php
+++ resources/views/shop/index.blade.php
@@ -5,92 +5,24 @@
 @endsection
 
 @section('content')
-    <div class="row">
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="https://ewedit.files.wordpress.com/2016/09/hpsorcstone.jpg?w=405"
-                     alt="..." class="img-responsive" >
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+    @foreach($products->chunk(3) as $productChunk)
+        <div class="row">
+            @foreach($productChunk as $product)
+                <div class="col-sm-6 col-md-4">
+                    <div class="thumbnail">
+                        <img src="{{ $product->imagePath }}"
+                             alt="..." class="img-responsive" >
+                        <div class="caption">
+                            <h3>{{ $product->title }}</h3>
+                            <p class="description">{{ $product->description }}</p>
+                            <div class="clearfix">
+                                <div class="pull-left price">${{ $product->price }}</div>
+                                <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endforeach
         </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="https://ewedit.files.wordpress.com/2016/09/hpsorcstone.jpg?w=405"
-                     alt="..." class="img-responsive" >
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="https://ewedit.files.wordpress.com/2016/09/hpsorcstone.jpg?w=405"
-                     alt="..." class="img-responsive" >
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="https://ewedit.files.wordpress.com/2016/09/hpsorcstone.jpg?w=405"
-                     alt="..." class="img-responsive" >
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="https://ewedit.files.wordpress.com/2016/09/hpsorcstone.jpg?w=405"
-                     alt="..." class="img-responsive" >
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-sm-6 col-md-4">
-            <div class="thumbnail">
-                <img src="https://ewedit.files.wordpress.com/2016/09/hpsorcstone.jpg?w=405"
-                     alt="..." class="img-responsive" >
-                <div class="caption">
-                    <h3>Product Title</h3>
-                    <p class="description">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores error eum inventore officia quis quos totam! Asperiores deleniti, distinctio illum incidunt nulla officiis quas suscipit vitae? Magni necessitatibus repellendus voluptate!</p>
-                    <div class="clearfix">
-                        <div class="pull-left price">$12</div>
-                        <a href="#" class="btn btn-success pull-right" role="button">Add to Cart</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @endforeach
 @endsection
