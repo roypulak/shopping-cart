5f565feae91ad4b77806a14f332cb4aa7e6e3a6c
Pulak Roy <roy1pulak@gmail.com>
2020-04-04 15:11:32 +0200

    [Lesson-13] storing orders in database

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Cart;
+use App\Order;
 use App\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
 use Stripe\Charge;
 use Stripe\Stripe;
@@ -70,14 +72,27 @@ class ProductController extends Controller
         $cart = new Cart($oldCart);
 
         try {
-            Charge::create([
+            $charge = Charge::create([
                 'amount' => $cart->totalPrice * 100,
                 'currency' => 'usd',
                 'source' => $request->input('stripeToken'), //obtained with Stripe.js
                 'description' => 'Test Charge'
             ]);
+
+            $this->saveOrder($request, $cart, $charge);
         } catch (\Exception $e) {
             return redirect()->route('checkout')->with('error', $e->getMessage());
         }
+    }
+
+    private function saveOrder(Request $request, Cart $cart, $charge)
+    {
+        $order = new Order();
+        $order->cart = serialize($cart);
+        $order->address = $request->input('address');
+        $order->name = $request->input('name');
+        $order->payment_id = $charge->id;
+
+        Auth::user()->orders()->save($order);
     }
 }
