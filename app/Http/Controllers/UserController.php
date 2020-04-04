1b8d4f9763ea9951bbe942dad1eb4548a1cb97fa
Pulak Roy <roy1pulak@gmail.com>
2020-04-04 18:30:06 +0200

    [Lesson-14][FIX] after logout it should direct to sign in route

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -74,6 +74,6 @@ class UserController extends Controller
     public function getLogout()
     {
         Auth::logout();
-        return redirect()->back();
+        return redirect()->route('user.signin');
     }
 }
