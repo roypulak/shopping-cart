0fc59cd43efa6914b7d5ac3ba35c78994d8c7ec2
Pulak Roy <roy1pulak@gmail.com>
2020-04-04 18:31:03 +0200

    [Lesson-14][FIX] route name

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -23,7 +23,7 @@ class Authenticate
                 return response('Unauthorized.', 401);
             } else {
                 Session::put('oldUrl', $request->url());
-                return redirect()->route('user');
+                return redirect()->route('user.signin');
             }
         }
 
